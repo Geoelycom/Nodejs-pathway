cb40634e13742a04f03eb5b8c0848e5e1b58c352
Elyan <ekenimohelyanpheta@gmail.com>
2022-08-01 09:39:03 -0700

    update package.json

--- Atemis/server/src/app.js
+++ Atemis/server/src/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const path = require('path');
 const planetsRouter = require('./Routes/planets/planets.router');
 const cors = require('cors');
 
 
 const app = express();
 app.use(express.json())
+// app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static('../server/public'))
+
+
 app.use(cors({
   origin: 'http://localhost:3000',
 }))
 app.use(planetsRouter)
-
+app.get('/', (req, res) => {
+  //res.sendFile('../server/public/index.html')
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
